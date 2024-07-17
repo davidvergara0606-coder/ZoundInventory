8041cd3e95aeb490cb26007cd8fa4cc89d34c78e
Jordan <jordan.castaeda@soy.sena.edu.co>
2024-07-17 17:03:01 -0500

    feat(back-end): Add DAL

--- Desktop/+/ConsoleApp1/ConsoleApp1/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Linq.Expressions;
-Console.WriteLine("Hello, World!");
--- Desktop/+/Orders/DAL/IFRepositorio.cs
+++ Desktop/+/Orders/DAL/IFRepositorio.cs
@@ -1,6 +1,8 @@
 ﻿using DAL.Datos;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -10,7 +12,7 @@ namespace DAL
 {
     public class IFRepositorio : IRepository
     {
-        //Llmada  a la clase de conetxto
+        //Llmada  a la clase de contexto
         ApplicationDbContext _context;
         //Dispose
         private bool disposedValue;
@@ -24,36 +26,77 @@ namespace DAL
                 await _context.SaveChangesAsync();
                 Result = toCreate;
             }
-            catch (Exception)
+            catch (DbException)
             {
                 throw;
             }
             return Result;
         }
         
-        public Task<bool> DeleteAsync<TEntity>(TEntity toDelete) where TEntity : class
+        public async Task<bool> DeleteAsync<TEntity>(TEntity toDelete) where TEntity : class
         {
-            throw new NotImplementedException();
+            bool Result = false;
+            try
+            {
+                _context.Entry<TEntity>(toDelete).State = EntityState.Deleted;
+                Result = await _context.SaveChangesAsync() > 0;
+            }
+            catch (DbException)
+            {
+                throw;
+            }
+            return Result;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if(_context != null)
+            {
+                _context.Dispose();
+            }
         }
 
-        public Task<List<TEntity>> FilterAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class
+        public async Task<List<TEntity>> FilterAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class
         {
-            throw new NotImplementedException();
+            List<TEntity> Result = default(List<TEntity>);
+            try
+            {
+                Result = await _context.Set<TEntity>().Where(criteria).ToListAsync();
+            }
+            catch (DbException)
+            {
+                throw;
+            }
+            return Result;
         }
 
-        public Task<TEntity> RetreiveAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class
+        public async Task<TEntity> RetreiveAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class
         {
-            throw new NotImplementedException();
+            TEntity Result = null;
+            try
+            {
+                Result = await _context.Set<TEntity>().FirstOrDefaultAsync(criteria);
+            }
+            catch (DbException)
+            {
+                throw;
+            }
+            return Result;
         }
 
-        public Task<bool> UpdateAsync<TEntity>(TEntity toUpdate) where TEntity : class
+        public async Task<bool> UpdateAsync<TEntity>(TEntity toUpdate) where TEntity : class
         {
-            throw new NotImplementedException();
+            bool Result = false;
+            try
+            {
+                _context.Entry<TEntity>(toUpdate).State = EntityState.Modified;
+                Result = await _context.SaveChangesAsync() > 0;
+            }
+            catch (DbException)
+            {
+                throw;
+            }
+            return Result;
         }
     }
 }
